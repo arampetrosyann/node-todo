ed6c2ed922b2ff104087c00c537566d6ca909775
arampetrosyan <0aramp@gmail.com>
2020-10-17 23:09:56 +0400

    [ci] update api controllers
    add validation

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,60 +1,74 @@
 const { remove, find } = require("lodash");
 const generateUniqueId = require("../public/javascripts/generateUniqueId.helper");
+const { wrapper } = require("../middlewares/error.middleware");
 
 const getId = generateUniqueId("do-");
 
-exports.addTodo = (req, res) => {
+exports.addTodo = wrapper((req, res) => {
   const { task } = req.body;
   const id = getId();
 
-  if (task.trim().length < 3) {
-    throw new Error("Minimum 3 characters");
+  const trimedTask = task.trim();
+
+  if (trimedTask.length < 3) {
+    throw new Error("Minimum 3 characters!");
+  } else if (trimedTask.length > 50) {
+    throw new Error("Maximum 50 characters!");
   }
 
-  const newTodo = { task, id };
+  const newTodo = { task: trimedTask, id };
 
   req.session.todos.push({ success: true, data: newTodo });
 
   res.status(200).json({ success: true, data: newTodo });
-};
+});
 
 exports.getTodo = (req, res) => {
-  const todoId = req.params.id;
+  const { id } = req.params;
 
   const todo = find(req.session.todos, (todo) => {
-    return todo.data.id === todoId;
+    return todo.data.id === id;
   });
 
   res.status(200).json(todo);
 };
 
 exports.getTodos = (req, res) => {
-  const todos = req.session.todos;
+  const { todos } = req.session;
 
   res.status(200).json(todos);
 };
 
-exports.updateTodo = (req, res) => {
-  const todoId = req.params.id;
+exports.updateTodo = wrapper((req, res, next) => {
+  const { task } = req.body;
+  const { id } = req.params;
+
+  const trimedTask = task.trim();
+
+  if (trimedTask.length < 3) {
+    throw new Error("Minimum 3 characters!");
+  } else if (trimedTask.length > 50) {
+    throw new Error("Maximum 50 characters!");
+  }
 
   const todo = find(req.session.todos, (todo) => {
-    return todo.data.id === todoId;
+    return todo.data.id === id;
   });
 
-  todo.data.task = req.body.task;
+  todo.data.task = trimedTask;
 
   res.status(200).json(todo);
-};
+});
 
 exports.deleteTodo = (req, res) => {
-  const todoId = req.params.id;
+  const { id } = req.params;
 
   const todo = find(req.session.todos, (todo) => {
-    return todo.data.id === todoId;
+    return todo.data.id === id;
   });
 
   remove(req.session.todos, (todo) => {
-    return todo.data.id === todoId;
+    return todo.data.id === id;
   });
 
   res.status(200).json(todo);
