2a28d3483a9070df8a3c63af9219433c671f4f0d
arampetrosyan <0aramp@gmail.com>
2020-10-17 23:07:47 +0400

    [ci] add error handler

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,6 +1,20 @@
+exports.wrapper = (func) => (req, res, next) => {
+  try {
+    func(req, res);
+  } catch (error) {
+    next(error);
+  }
+};
+
 exports.notFoundMiddle = (req, res, next) => {
   res.status(404).render("error", {
     errHead: "Not Found",
     errBody: `The requested URL ${req.url} was not found on this server.`,
   });
+
+  next();
+};
+
+exports.errorHandler = (err, req, res, next) => {
+  res.status(400).send(err.message);
 };
