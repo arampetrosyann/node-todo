0fda6860e59482d8f5d7dbf5ffa374e0b0caa7b8
arampetrosyan <0aramp@gmail.com>
2020-10-17 23:11:14 +0400

    [ci] separate controllers

--- routes/api.v1.js
+++ routes/api.v1.js
@@ -8,8 +8,8 @@ const {
   deleteTodo,
 } = require("../controllers/api.controller");
 
-api.route("/api").get(getTodos).post(addTodo);
+api.route("/api/todos").get(getTodos).post(addTodo);
 
-api.route("/api/:id").get(getTodo).put(updateTodo).delete(deleteTodo);
+api.route("/api/todos/:id").get(getTodo).put(updateTodo).delete(deleteTodo);
 
 module.exports = api;
